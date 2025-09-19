898ec98a5c1eed2f0bf2ab0759175f6c9d8a7cea
samarmeena9920 <150104404+samarmeena9920@users.noreply.github.com>
2025-09-20 03:31:00 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-        await mongoose.connect('mongodb+srv://samarmeena9920:Sambahadur9920@cluster0.2uhbwy9.mongodb.net/FoodDelivery')
+        await mongoose
+    .connect(
+      process.env.MongoDB_URL
+    )
         .then(() =>
             console.log("MongoDB connected")); 
         }
