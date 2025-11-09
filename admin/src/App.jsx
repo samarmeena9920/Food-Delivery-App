97b324d27ab668a5586d4675925fe9d07fd20a94
samarmeena9920 <150104404+samarmeena9920@users.noreply.github.com>
2025-11-09 20:28:03 +0530

    Update API URL for production environment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,8 +11,8 @@ import StoreContextProvider from "./context/StoreContext";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  //const url = "https://susangat-food-del-backend.onrender.com"; // Updated URL for production
-  const url = "http://localhost:4000"; // Comment out the local URL
+  const url = "https://food-delivery-app-backend-066k.onrender.com"; // Updated URL for production
+  //const url = "http://localhost:4000"; // Comment out the local URL
   return (
     <StoreContextProvider>
       <ToastContainer />
